cc8ef09e1ad5c88c1762cbc0510aaa39d55a1900
Eduardo Nicioli <eduardo.nicioli@evino.com.br>
2020-05-21 22:34:46 -0300

    test improvements

--- test/controllers/RouteController.js
+++ test/controllers/RouteController.js
@@ -2,45 +2,147 @@ const fs = require('fs')
 const assert = require('assert')
 const chai = require('chai')
 const chaiHttp = require('chai-http')
+const mockKnex = require('mock-knex');
+const tracker = mockKnex.getTracker();
 const app = require('./../../src/app')
 
 chai.use(chaiHttp)
 
+const fixtureRoute = readFixtureJSONFile('route.json')
+
 describe('RouteController', () => {
     describe('#post /route', () => {
-        it('should 201', (done) => {
+        before(() => {
+            tracker.install()
+            tracker.on('query', (query, step) => {
+                [
+                    () => {
+                        query.response([1])
+                    },
+                    () => {
+                        query.response(fixtureRoute)
+                    }
+                ][step - 1]()
+            })
+        })
+        it('should return 201', (done) => {
             chai
             .request(app)
-            .post(`/route`)
-            .send(readFixtureJSONFile('route.json'))
+            .post('/route')
+            .send(fixtureRoute)
             .end((err, res) => {
                 assert.equal(res.status, 201)
                 done()
             })
         })
+        after(() => {
+            tracker.uninstall()
+        })
     })
 
     describe('#get /route/:origin/:destination', () => {
-        it('should return 200', (done) => {
-            const route = readFixtureJSONFile('route.json')
-            chai
-            .request(app)
-            .get(`/route/${route.origin}/${route.destination}`)
-            .end((err, res) => {
-                assert.equal(res.status, 200)
-                done()
+        context('existent route', () => {
+            before(() => {
+                tracker.install()
+                tracker.on('query', (query, step) => {
+                    [
+                        () => {
+                            query.response(fixtureRoute)
+                        }
+                    ][step - 1]()
+                })
+            })
+            it('should return 200', (done) => {
+                chai
+                .request(app)
+                .get(`/route/${fixtureRoute.origin}/${fixtureRoute.destination}`)
+                .end((err, res) => {
+                    assert.equal(res.status, 200)
+                    done()
+                })
+            })
+            after(() => {
+                tracker.uninstall()
+            })
+        })
+
+        context('nonexistent route', () => {
+            before(() => {
+                tracker.install()
+                tracker.on('query', (query, step) => {
+                    [
+                        () => {
+                            query.response(undefined)
+                        }
+                    ][step - 1]()
+                })
+            })
+            it('should return 404', (done) => {
+                chai
+                .request(app)
+                .get(`/route/XXX/ZZZ`)
+                .end((err, res) => {
+                    assert.equal(res.status, 404)
+                    done()
+                })
+            })
+            after(() => {
+                tracker.uninstall()
             })
         })
     })
 
-    describe('#get /route/:origin/:destination nonexistent', () => {
-        it('should return 404', (done) => {
-            chai
-            .request(app)
-            .get(`/route/XXX/ZZZ`)
-            .end((err, res) => {
-                assert.equal(res.status, 404)
-                done()
+    describe('#delete /route/:origin/:destination', () => {
+        context('existent route', () => {
+            before(() => {
+                tracker.install()
+                tracker.on('query', (query, step) => {
+                    [
+                        () => {
+                            query.response(fixtureRoute)
+                        },
+                        () => {
+                            query.response(1)
+                        }
+                    ][step - 1]()
+                })
+            })
+            it('should return 204', (done) => {
+                chai
+                .request(app)
+                .delete(`/route/${fixtureRoute.origin}/${fixtureRoute.destination}`)
+                .end((err, res) => {
+                    assert.equal(res.status, 204)
+                    done()
+                })
+            })
+            after(() => {
+                tracker.uninstall()
+            })
+        })
+
+        context('nonexistent route', () => {
+            before(() => {
+                tracker.install()
+                tracker.on('query', (query, step) => {
+                    [
+                      () => {
+                        query.response(undefined)
+                      }
+                    ][step - 1]()
+                })
+            })
+            it('should return 404', (done) => {
+                chai
+                .request(app)
+                .delete('/route/XXX/ZZZ')
+                .end((err, res) => {
+                    assert.equal(res.status, 404)
+                    done()
+                })
+            })
+            after(() => {
+                tracker.uninstall()
             })
         })
     })
--- test/controllers/StopoverController.js
+++ test/controllers/StopoverController.js
@@ -2,32 +2,81 @@ const fs = require('fs')
 const assert = require('assert')
 const chai = require('chai')
 const chaiHttp = require('chai-http')
+const mockKnex = require('mock-knex');
+const tracker = mockKnex.getTracker();
 const app = require('./../../src/app')
-const connection = require('./../../src/database/connection')
 
 chai.use(chaiHttp)
 
+const fixtureRoute = readFixtureJSONFile('route.json')
+
 describe('StopoverController', () => {
-    context('#get /stopover/:origin/:destination (origin = destination)', () => {
-        it('it should return 400', (done) => {
-            chai
-            .request(app)
-            .get('/stopover/XXX/XXX')
-            .end((err, res) => {
-                assert.equal(res.status, 400)
-                done()
+    describe('#get /stopover/:origin/:destination', () => {
+        context('existent route', () => {
+            before(() => {
+                tracker.install()
+                tracker.on('query', (query, step) => {
+                    [
+                        () => {
+                            query.response([fixtureRoute])
+                        },
+                        () => {
+                            query.response([fixtureRoute])
+                        },
+                        () => {
+                            query.response([fixtureRoute])
+                        }
+                    ][step - 1]()
+                })
+            })
+            it('should return 200', (done) => {
+                chai
+                .request(app)
+                .get(`/stopover/${fixtureRoute.origin}/${fixtureRoute.destination}`)
+                .end((err, res) => {
+                    assert.equal(res.status, 200)
+                    done()
+                })
+            })
+            after(() => {
+                tracker.uninstall()
             })
         })
-    })
 
-    context('#get /stopover/:origin/:destination (nonexistent)', () => {
-        it('it should return 400', (done) => {
-            chai
-            .request(app)
-            .get('/stopover/XXX/ZZZ')
-            .end((err, res) => {
-                assert.equal(res.status, 404)
-                done()
+        context('origin == destination', () => {
+            it('should return 400', (done) => {
+                chai
+                .request(app)
+                .get('/stopover/XXX/XXX')
+                .end((err, res) => {
+                    assert.equal(res.status, 400)
+                    done()
+                })
+            })
+        })
+
+        context('nonexistent route', () => {
+            before(() => {
+                tracker.install()
+                tracker.on('query', (query, step) => {
+                    [
+                        () => {
+                            query.response(undefined)
+                        }
+                    ][step - 1]()
+                })
+            })
+            it('should return 400', (done) => {
+                chai
+                .request(app)
+                .get('/stopover/XXX/ZZZ')
+                .end((err, res) => {
+                    assert.equal(res.status, 404)
+                    done()
+                })
+            })
+            after(() => {
+                tracker.uninstall()
             })
         })
     })
